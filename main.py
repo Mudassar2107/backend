7098f5d683ff35a3dbc153882ef8488dad847075
Mudassar Sayeed <148698944+Mudassar2107@users.noreply.github.com>
2026-07-14 16:22:38 +0530

    feat: add PUT endpoint for updating todo items

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ async def create_todo(new_task: TodoItem):
     task_dict = new_task.dict()
     fake_todo_db.append(task_dict)
     return{"Message": "Task successfully created!", "Tasks":task_dict}
+@app.put("/todos/{task_id}")
+async def update_todo(task_id: int, updated_task: TodoItem):
+    for i in range(len(fake_todo_db)):
+        if fake_todo_db[i]["id"] == task_id:
+            fake_todo_db[i] = updated_task.dict()
+            return{"Message": f"Task {task_id} updated!","Task": fake_todo_db[i]}
+    return{"Error": "Task not found"}
