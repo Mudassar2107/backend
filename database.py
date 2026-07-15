4d92d0972f615d9713502dbf30bd7557feb44681
Mudassar Sayeed <148698944+Mudassar2107@users.noreply.github.com>
2026-07-15 19:05:19 +0530

    feat: initialize database session and declarative base model for SQLAlchemy

--- database.py
+++ database.py
@@ -7,3 +7,9 @@ SQLACLCHEMY__URL = "postgresql://mudassar:postgres@localhost:5432/todo_db"
 
 #2. The Engine (The actual connection cable)
 engine = create_engine(SQLACLCHEMY__URL)
+
+# 3. The Session (The Waiting Room)
+SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind=engine)
+
+# 4. The Base (The Blueprint Foundation)
+Base = declarative_base()
