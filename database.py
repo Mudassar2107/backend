3819bb0b5b48fccdc40333b0d74897203c76e85a
Mudassar Sayeed <148698944+Mudassar2107@users.noreply.github.com>
2026-07-15 19:00:45 +0530

    feat: initialize SQLAlchemy engine and session configuration for PostgreSQL database connection

--- database.py
+++ database.py
@@ -1,1 +1,9 @@
-from
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 1. The Connection String (The exact coordinates to your database)
+SQLACLCHEMY__URL = "postgresql://mudassar:postgres@localhost:5432/todo_db"
+
+#2. The Engine (The actual connection cable)
+engine = create_engine(SQLACLCHEMY__URL)
